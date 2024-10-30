4c33d7591a9dabd9d05016fe8cee5be98245d696
KyleCan81 <Kylecan8112@gmail.com>
2024-10-30 10:35:31 -0400

    Change Theme

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function changeList() {
+    const lists = [
+        { radioId: "Sweet", listId: "Sweet-List"},
+        { radioId: "Savory", listId: "Savory-List"}
+    ]
+
+//Unfortunately really stumped today.
